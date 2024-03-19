d17f90978464e21fd62bdb52ccf25890e55f2999
sreekanthreddy4000 <sreekanthreddykalluri3@gmail.com>
2024-03-19 10:39:11 +0000

    solution

---
+++ src/components/TextEditor/index.js
@@ -0,0 +1,82 @@
+import {Component} from 'react'
+import {VscBold, GoItalic, AiOutlineUnderline} from 'react-icons/all'
+import {Container, ButtonsList, Button, TextArea} from './styledComponents'
+
+
+class TextEditor extends Component {
+  state = {
+    isBold: false,
+    isItalic: false,
+    isUnderline: false,
+  }
+
+  handleBoldClick = () => {
+    this.setState(prevState => ({
+      isBold: !prevState.isBold,
+    }))
+  }
+
+  handleItalicClick = () => {
+    this.setState(prevState => ({
+      isItalic: !prevState.isItalic,
+    }))
+  }
+
+  handleUnderlineClick = () => {
+    this.setState(prevState => ({
+      isUnderline: !prevState.isUnderline,
+    }))
+  }
+
+  render() {
+    const {isBold, isItalic, isUnderline} = this.state
+
+    return (
+      <Container>
+        <h1>Text Editor</h1>
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/text-editor-img.png"
+          alt="text editor"
+        />
+        <ButtonsList>
+          <li>
+            <Button
+              isActive={isBold}
+              onClick={this.handleBoldClick}
+              data-testid="bold"
+            >
+              <VscBold />
+            </Button>
+          </li>
+          <li>
+            <Button
+              isActive={isItalic}
+              onClick={this.handleItalicClick}
+              data-testid="italic"
+            >
+              <GoItalic />
+            </Button>
+          </li>
+          <li>
+            <Button
+              isActive={isUnderline}
+              onClick={this.handleUnderlineClick}
+              data-testid="underline"
+            >
+              <AiOutlineUnderline />
+            </Button>
+          </li>
+        </ButtonsList>
+        <TextArea
+          style={{
+            fontWeight: isBold ? 'bold' : 'normal',
+            fontStyle: isItalic ? 'italic' : 'normal',
+            textDecoration: isUnderline ? 'underline' : 'none',
+          }}
+        />
+      </Container>
+    )
+  }
+}
+
+export default TextEditor
